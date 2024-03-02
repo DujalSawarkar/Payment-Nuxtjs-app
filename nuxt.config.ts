013cbab2ff6c2c38f1753838f4f9bbc581f0d95f
DujalSawarkar <dujalsawar5@gmail.com>
2024-03-02 10:10:16 +0530

    initialize

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@vueuse/nuxt", "@element-plus/nuxt"],
+  css: ["~/assets/css/style.css"],
+});
